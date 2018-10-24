dd06f10a28610934364df3ac3fb6d994206850a5
maqing01 <475986855@qq.com>
2018-10-24 11:23:37 +0800

    H5 postion:fixed

--- src/helper.ts
+++ src/helper.ts
@@ -1,8 +1,25 @@
+const controlBodyScroll = (function (): (parm: boolean) => void {
+    let bodyEl = document.body
+    let top = 0
+    return function (isFixed) {
+        if (isFixed) {
+            top = window.scrollY
+
+            bodyEl.style.position = 'fixed'
+            bodyEl.style.top = -top + 'px'
+        } else {
+            bodyEl.style.position = ''
+            bodyEl.style.top = ''
+
+            window.scrollTo(0, top) // 回到原先的top
+        }
+    }
+})()
+
 interface HelperType {
     afterOpen(): void;
     beforeClose(): void;
 }
-
 export const helper: HelperType = (function (bodyCls) {
 
     return {
@@ -11,7 +28,8 @@ export const helper: HelperType = (function (bodyCls) {
 
             if (document.querySelectorAll('.enter-animation').length > 0) return;
 
-            document.body.classList.add(bodyCls);
+            // document.body.classList.add(bodyCls);
+            controlBodyScroll(true)
 
         },
 
@@ -20,7 +38,8 @@ export const helper: HelperType = (function (bodyCls) {
             //mask隐藏时恢复底层滚动，只有在最后一层mask隐藏时处理
             if (document.querySelectorAll('.enter-animation').length !== 1) return;
 
-            document.body.classList.remove(bodyCls);
+            // document.body.classList.remove(bodyCls);
+            controlBodyScroll(false)
 
         }
 
